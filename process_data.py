39cb1f81686a7415227080c04966aee621ffb618
xiaokeji1 <115883248+xiaokeji1@users.noreply.github.com>
2024-05-18 21:12:12 +1000

    add nots process_data.py

--- process_data.py
+++ process_data.py
@@ -1,13 +1,13 @@
 
 import numpy as np
 import re
-# 读取文件
+# Read the file
 with open("/root/sentiment_analysisB/process_data/data.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
-# 定义正则表达式模式
+# Define regular expression pattern
 pattern = r"__label__(\d+)\s+(.+)"
 
-# 处理数据
+# Data processing
 data = []
 m, x, y, total = 0, 0, 0, 0
 for line in lines:
@@ -26,11 +26,11 @@ for line in lines:
 print(m, x, y, total)
 import time
 # time.sleep(150000)
-# 打乱数据
+# Shuffle the data
 np.random.seed(42)
 np.random.shuffle(data)
 
-# 划分数据集
+# Divide the dataset
 total_samples = len(data)
 train_size = int(0.6 * total_samples)
 val_size = int(0.2 * total_samples)
@@ -40,7 +40,7 @@ val_data = np.array(data[train_size:train_size+val_size])
 test_data = np.array(data[train_size+val_size:])
 print("examine train data", train_data.shape, type(train_data))
 
-# 分离label和text
+# Separate label and text
 train_label, train_text = train_data[:, 0], train_data[:, 1]
 val_label, val_text = val_data[:, 0], val_data[:, 1]
 test_label, test_text = test_data[:, 0], test_data[:, 1]
@@ -50,12 +50,12 @@ train_label, val_label, test_label = train_label.astype(int), val_label.astype(i
 print("example train label dtype", type(train_label[0]))
 
 
-# 显示数据大小
+# Display data size
 print("训练集大小:", len(train_data))
 print("验证集大小:", len(val_data))
 print("测试集大小:", len(test_data))
 
-# 将选取的元素保存为一个新的.npy文件  
+# Save the selected elements as a new .npy file 
 np.save('/root/sentiment_analysisB/process_data/train_text.npy', train_text)  
 np.save('/root/sentiment_analysisB/process_data/train_label.npy', train_label)  
 np.save('/root/sentiment_analysisB/process_data/val_text.npy', val_text)  
